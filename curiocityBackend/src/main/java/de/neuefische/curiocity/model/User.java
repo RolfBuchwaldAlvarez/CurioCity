b31f3cd10e13ce1c03ff92d8e69b9771c0a7cf39
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-07-16 12:02:09 +0200

    Add class User

---
+++ curiocityBackend/src/main/java/de/neuefische/curiocity/model/User.java
@@ -0,0 +1,19 @@
+package de.neuefische.curiocity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+  private String userId;
+  private String firstName;
+  private String lastName;
+  private String eMail;
+  private String yearOfBirth;
+  private String countryOfResidence;
+  private String postalCode;
+  private String password;
+}
