f1a9c0181aefa750af55e3cfe01590a2ffd69dd2
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-07-16 23:30:20 +0200

    Add PutMapping addUser

--- curiocityBackend/src/main/java/de/neuefische/curiocity/controller/userController.java
+++ curiocityBackend/src/main/java/de/neuefische/curiocity/controller/userController.java
@@ -1,24 +1,32 @@
 package de.neuefische.curiocity.controller;
 
 import de.neuefische.curiocity.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class userController {
 
-  public List<User> userList = List.of(
-      new User("123abc", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "50667", "123456"),
-      new User("234bcd", "Nikita", "Thomson", "xyz@gMail.de", "female", "2002", "Japan", "G-123-60", "abcdef")
-  );
+  public List<User> userList = new ArrayList<>();
+      /*List.of(
+      new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "50667", "123"),
+      new User("2", "Nikita", "Thomson", "bcd@eMail.de", "female", "2002", "Japan", "12345", "234")
+  )*/;
 
   @GetMapping
   public List<User> getUsers() {
     return userList;
   }
+
+  @PutMapping // Funzt mit List = List.of( new User...); nicht. Warum?
+  public User addUser(@RequestBody User user) {
+    userList.add(user);
+    return userList.get(userList.size()-1);
+  }
+
+  asf
 
 }
