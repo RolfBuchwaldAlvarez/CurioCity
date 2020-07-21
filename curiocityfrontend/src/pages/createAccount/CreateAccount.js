ff066728e6f14e57f38f1c6a6ebe126d26972e40
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-07-22 01:52:47 +0200

    Add some minor changes to create account page

--- curiocityfrontend/src/pages/createAccount/CreateAccount.js
+++ curiocityfrontend/src/pages/createAccount/CreateAccount.js
@@ -23,11 +23,18 @@ const useStyles = makeStyles((theme) => ({
   wrapperBox: {
     backgroundColor: "rgba(32,65,90,0.6)",
     width: "100%",
-    height: "70%",
+    height: "72%",
   },
   typo: {
     color: "white",
     fontSize: "24px",
+    margin: theme.spacing(2),
+  },
+  textfieldBox: {
+    backgroundColor: "rgba(255,255,255,0.1)",
+    borderRadius: "4px",
+    border: "solid white 1px",
+    padding: theme.spacing(2),
   },
   textfield: {
     margin: theme.spacing(1),
@@ -48,17 +55,20 @@ export default function CreateAccount() {
       <Box className={classes.container}>
         <Box display="flex" flexDirection="column" justifyContent="flex-end" alignItems="center"
              className={classes.layerInBetweenBox}>
-          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center"
+          <Box display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center"
                className={classes.wrapperBox}>
             <Typography className={classes.typo}>Create New Account</Typography>
-            <TextField rid="standard-basic" label="First Name" className={classes.textfield}/>
-            <TextField rid="standard-basic" label="Last Name" className={classes.textfield}/>
-            <TextField rid="standard-basic" label="E-Mail Address" className={classes.textfield}/>
-            <TextField rid="standard-basic" label="Gender" className={classes.textfield}/>
-            <TextField rid="standard-basic" label="Year of Birth" className={classes.textfield}/>
-            <TextField rid="standard-basic" label="Country of Residence" className={classes.textfield}/>
-            <TextField rid="standard-basic" label="Postal Code" className={classes.textfield}/>
-            <TextField rid="standard-basic" label="Password" className={classes.textfield}/>
+            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center"
+                 className={classes.textfieldBox}>
+              <TextField rid="standard-basic" label="First Name" className={classes.textfield}/>
+              <TextField rid="standard-basic" label="Last Name" className={classes.textfield}/>
+              <TextField rid="standard-basic" label="E-Mail Address" className={classes.textfield}/>
+              <TextField rid="standard-basic" label="Gender" className={classes.textfield}/>
+              <TextField rid="standard-basic" label="Year of Birth" className={classes.textfield}/>
+              <TextField rid="standard-basic" label="Country of Residence" className={classes.textfield}/>
+              <TextField rid="standard-basic" label="Postal Code" className={classes.textfield}/>
+              <TextField rid="standard-basic" label="Password" className={classes.textfield}/>
+            </Box>
             <Button variant="contained" color="secondary" className={classes.button}>
               CONFIRM
             </Button>
