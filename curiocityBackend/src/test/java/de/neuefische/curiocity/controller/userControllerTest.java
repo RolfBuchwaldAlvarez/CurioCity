8494911a01001612e9ccb095d9a5a698354ebec0
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-07-22 12:56:17 +0200

    Add @Autowired UserDb + @BeforeEach resetDb() + update GetMapping tests accordingly

--- curiocityBackend/src/test/java/de/neuefische/curiocity/controller/userControllerTest.java
+++ curiocityBackend/src/test/java/de/neuefische/curiocity/controller/userControllerTest.java
@@ -1,6 +1,8 @@
 package de.neuefische.curiocity.controller;
 
+import de.neuefische.curiocity.db.UserDb;
 import de.neuefische.curiocity.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,10 +24,19 @@ class userControllerTest {
   @Autowired
   private TestRestTemplate restTemplate;
 
+  @Autowired
+  UserDb userDb;
+
+  @BeforeEach
+  public void resetDb() {
+    userDb.clearDb();
+  }
+
   @Test
   public void getUserShouldReturnAllUsers() {
     //GET
     String url = "http://localhost:" + port + "/api/users";
+    userDb.addUser(new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "23456", "123"));
 
     //When
     ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
@@ -35,15 +46,16 @@ class userControllerTest {
     //Then
     assertEquals(statusCode, HttpStatus.OK);
     assert users != null;
+    assertEquals(1, users.length);
     assertEquals(users[0], new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "23456", "123"));
-    assertEquals(users[1], new User("2", "Rene", "Koch", "cde@eMail.de", "male", "1987", "Netherlands", "34567", "345"));
-    assertEquals(users[2], new User("3", "Nikita", "Thomson", "bcd@eMail.de", "female", "2002", "Japan", "12345", "234"));
   }
 
   @Test
   public void getUserWithQueryEqualToOneShouldReturnUserWithIdOne() {
     //GET
     String url = "http://localhost:" + port + "/api/users?q=1";
+    userDb.addUser(new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "23456", "123"));
+    userDb.addUser(new User("2", "Rene", "Koch", "cde@eMail.de", "male", "1987", "Netherlands", "34567", "345"));
 
     //When
     ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
@@ -53,6 +65,7 @@ class userControllerTest {
     //Then
     assertEquals(statusCode, HttpStatus.OK);
     assert users != null;
+    assertEquals(1, users.length);
     assertEquals(users[0], new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "23456", "123"));
   }
 
@@ -60,6 +73,8 @@ class userControllerTest {
   public void getUserWithQueryEqualToTwoShouldReturnUserWithIdTwo() {
     //GET
     String url = "http://localhost:" + port + "/api/users?q=2";
+    userDb.addUser(new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "23456", "123"));
+    userDb.addUser(new User("2", "Rene", "Koch", "cde@eMail.de", "male", "1987", "Netherlands", "34567", "345"));
 
     //When
     ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
@@ -69,6 +84,7 @@ class userControllerTest {
     //Then
     assertEquals(statusCode, HttpStatus.OK);
     assert users != null;
+    assertEquals(1, users.length);
     assertEquals(users[0], new User("2", "Rene", "Koch", "cde@eMail.de", "male", "1987", "Netherlands", "34567", "345"));
   }
 
@@ -76,6 +92,8 @@ class userControllerTest {
   public void getUserByLeavingQueryEmptyShouldReturnAllUsers() {
     //GET
     String url = "http://localhost:" + port + "/api/users?q=";
+    userDb.addUser(new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "23456", "123"));
+    userDb.addUser(new User("2", "Rene", "Koch", "cde@eMail.de", "male", "1987", "Netherlands", "34567", "345"));
 
     //When
     ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
@@ -85,11 +103,12 @@ class userControllerTest {
     //Then
     assertEquals(statusCode, HttpStatus.OK);
     assert users != null;
+    assertEquals(2, users.length);
     assertEquals(users[0], new User("1", "Rolf", "Buchwald", "abc@eMail.de", "male", "1983", "Germany", "23456", "123"));
     assertEquals(users[1], new User("2", "Rene", "Koch", "cde@eMail.de", "male", "1987", "Netherlands", "34567", "345"));
-    assertEquals(users[2], new User("3", "Nikita", "Thomson", "bcd@eMail.de", "female", "2002", "Japan", "12345", "234"));
   }
 
+  //Split tests into 2 (add new user + return new user)
   @Test
   public void addUserShouldAddNewUserToUserList() {
     //PUT
@@ -113,8 +132,6 @@ class userControllerTest {
     assertEquals(users[2], new User("3", "Nikita", "Thomson", "bcd@eMail.de", "female", "2002", "Japan", "12345", "234"));
     assertEquals(users[3], new User("4", "Robbi", "Cobain", "def@eMail.de", "male", "2014", "Greece", "45678", "456"));
   }
-
-  // How to test with Optionals ???
 
   @Test
   public void getUserByIdShouldReturnUserThatBelongsToGivenId() {
