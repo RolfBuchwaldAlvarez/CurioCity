8dd4262913344b813c3c13379b44b2edec4a4cc8
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-07-27 18:37:43 +0200

    Solve merge conflict

--- curiocityfrontend/src/App.js
+++ curiocityfrontend/src/App.js
@@ -1,13 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import CreateAccount from "./pages/createAccount/CreateAccount";
-
-
-function App() {
-  return (
-    /*<UserLoginSF/>*/
-    <CreateAccount/>
-=======
 import UserLoginSF from "./pages/userLoginSimplified/UserLoginSF";
 import CreateAccount from "./pages/createAccount/CreateAccount";
 import {Route, Switch} from "react-router-dom";
@@ -22,7 +13,6 @@ function App() {
         <UserLoginSF/>
       </Route>
     </Switch>
->>>>>>> db0474a82ed498b3063fa227a0b5a8aa85325be7
   );
 }
 
