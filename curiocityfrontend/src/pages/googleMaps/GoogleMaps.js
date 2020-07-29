365683d35be4953d514a1286bed9117a630c68cd
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-07-29 23:19:13 +0200

    Show GoogleMaps on main page

--- curiocityfrontend/src/pages/googleMaps/GoogleMaps.js
+++ curiocityfrontend/src/pages/googleMaps/GoogleMaps.js
@@ -1,6 +1,41 @@
 import React from "react";
+import {GoogleMap, useLoadScript} from "@react-google-maps/api";
+import MapStyles from "../../utils/MapStyles";
 
+const libraries = ["places"]
+const mapContainerStyle = {
+  width: "100vw",
+  height: "551px",
+};
+const center = {
+  lat: 50.937531,
+  lng: 6.960279,
+};
+const options = {
+  styles: MapStyles,
+}
 
 export default function GoogleMaps() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
 
+  if (loadError) {
+    return "Error loading maps"
+  }
+  if (!isLoaded) {
+    return "Loading maps"
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={14}
+        center={center}
+        options={options}
+      />
+    </div>
+  );
 }
--- curiocityfrontend/src/pages/mainPage/MainPage.js
+++ curiocityfrontend/src/pages/mainPage/MainPage.js
@@ -2,17 +2,9 @@ import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import MainPageHeader from "./components/MainPageHeader";
 import MainPageBottomNavigation from "./components/MainPageBottomNavigation";
-import Box from "@material-ui/core/Box";
+import GoogleMaps from "../googleMaps/GoogleMaps";
 
 const useStyles = makeStyles((theme) => ({
-  mainPage: {
-    height: "100vh",
-    width: "100%",
-    backgroundImage: "url(/images/MapCologneDoubleSize.png)",
-    backgroundPosition: "center", /* Center the image */
-    backgroundRepeat: "no-repeat", /* Do not repeat the image */
-    backgroundSize: "cover", /* Resize the background image to cover the entire container */
-  },
   box1: {
     height: "50%",
   },
@@ -26,14 +18,9 @@ export default function MainPage() {
 
   return (
     <>
-      <Box className={classes.mainPage} display="flex" flexDirection="column">
-        <Box className={classes.box1} display="flex" flexDirection="column" justifyContent="flex-start">
-          <MainPageHeader/>
-        </Box>
-        <Box className={classes.box2} display="flex" flexDirection="column" justifyContent="flex-end">
-          <MainPageBottomNavigation/>
-        </Box>
-      </Box>
+      <MainPageHeader/>
+      <GoogleMaps/>
+      <MainPageBottomNavigation/>
     </>
   );
 }
