95bd1c48fe8092425c754f4d1fcf2828c62a2363
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-07-30 00:41:53 +0200

    Build display green markers on map, show info-window on click, and close info-window

--- curiocityfrontend/src/pages/googleMaps/GoogleMaps.js
+++ curiocityfrontend/src/pages/googleMaps/GoogleMaps.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {GoogleMap, useLoadScript} from "@react-google-maps/api";
+import {GoogleMap, InfoWindow, Marker, useLoadScript} from "@react-google-maps/api";
 import MapStyles from "../../utils/MapStyles";
+import {formatRelative} from "date-fns";
 
 const libraries = ["places"]
 const mapContainerStyle = {
@@ -13,6 +14,8 @@ const center = {
 };
 const options = {
   styles: MapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
 }
 
 export default function GoogleMaps() {
@@ -20,6 +23,26 @@ export default function GoogleMaps() {
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
+
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((event) => {
+    console.log(event);
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map; //to avoid re-renders
+  }, []);
 
   if (loadError) {
     return "Error loading maps"
@@ -35,7 +58,40 @@ export default function GoogleMaps() {
         zoom={14}
         center={center}
         options={options}
-      />
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map(marker => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{lat: marker.lat, lng: marker.lng}}
+            icon={{
+              url: "/svg/greenMarker.svg",
+              scaledSize: new window.google.maps.Size(14, 14),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(7, 7),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{lat: selected.lat, lng: selected.lng}}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Restaurant</h2>
+              <p>A cozy looking italian diner!</p>
+              <p>{formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
   );
 }
