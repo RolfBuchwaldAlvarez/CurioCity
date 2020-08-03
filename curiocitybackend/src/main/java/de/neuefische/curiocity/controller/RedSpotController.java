baebf318aef80953303e2082c4900603b19e9625
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-08-04 00:12:31 +0200

    Create RedSpotController and add same functionality as in GreenspotController

---
+++ curiocitybackend/src/main/java/de/neuefische/curiocity/controller/RedSpotController.java
@@ -0,0 +1,41 @@
+package de.neuefische.curiocity.controller;
+
+import de.neuefische.curiocity.model.Spot;
+import de.neuefische.curiocity.service.SpotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/redSpots")
+public class RedSpotController {
+
+  private final SpotService spotService;
+
+  @Autowired
+  public GreenSpotController(SpotService spotService) {
+    this.spotService = spotService;
+  }
+
+  @GetMapping
+  public List<Spot> getSpots() {
+    return spotService.getSpots();
+  }
+
+  @PutMapping
+  public Spot addSpot(@RequestBody Spot spot) {
+    return spotService.addSpot(spot);
+  }
+
+  @DeleteMapping
+  public void deleteSpot(@RequestBody Spot spot) {
+    String id = spot.getId();
+    if (!spotService.deleteSpot(id)) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Spot not found");
+    }
+  }
+
+}
