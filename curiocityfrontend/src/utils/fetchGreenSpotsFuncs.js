6325f2a03d8f5d0f1dbac2792cf587fa3c6ecf13
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-08-04 02:47:39 +0200

    Outsource outcommented putFetch-functionality to fetchGreenSpotsFuncs.js

--- curiocityfrontend/src/utils/fetchGreenSpotsFuncs.js
+++ curiocityfrontend/src/utils/fetchGreenSpotsFuncs.js
@@ -1,4 +1,6 @@
 // get all green spots
+import React from "react";
+
 export async function fetchAllGreenSpots() {
   const response = await fetch('/api/greenSpots', {
     method: 'GET',
@@ -24,3 +26,22 @@ export async function putGreenSpot(id, category, lat, lng) {
   }
   return await response.json();
 }
+
+// temporär aus GoolgeMaps.js ausgelagert
+
+/*
+const onMapClick = React.useCallback((event) => {
+  console.log(event);
+  putGreenSpot(event.id, event.category, event.latLng.lat(), event.latLng.lng())
+    .then((spot) => {
+      setGreenSpots((current) => [
+        ...current,
+        {
+          id: event.id,
+          category: event.category,
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng(),
+          /!*time: new Date(),*!/
+        }]);
+    })
+}, []);*/
